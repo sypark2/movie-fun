d0df53fafe5733b4c162f1856ced52cd91872055
sjlee5 <sjlee5@lsis.com>
2019-08-21 11:51:58 +0900

    multi database

--- src/main/java/org/superbiz/moviefun/DatabaseConfig.java
+++ src/main/java/org/superbiz/moviefun/DatabaseConfig.java
@@ -1,25 +1,21 @@
 package org.superbiz.moviefun;
 
-import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.orm.jpa.JpaDialect;
-import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
-import javax.xml.crypto.Data;
-
+@Configuration
 public class DatabaseConfig {
 
     @Bean
-    DatabaseServiceCredentials serviceCredentials(@Value("${vcap.services}")
-                                                        String vcapServices){
-        return new DatabaseServiceCredentials(vcapServices);
+    DatabaseServiceCredentials serviceCredentials(@Value("${vcap.services}") String vcapSerivces) {
+        return new DatabaseServiceCredentials(vcapSerivces);
     }
 
     @Bean
-    HibernateJpaVendorAdapter jpaVendorAdapter(){
+    HibernateJpaVendorAdapter jpaVendorAdapter() {
         HibernateJpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
         jpaVendorAdapter.setDatabase(Database.MYSQL);
         jpaVendorAdapter.setDatabasePlatform("org.hibernate.dialect.MySQL5Dialect");
--- src/main/java/org/superbiz/moviefun/movies/MoviesDatabaseConfig.java
+++ src/main/java/org/superbiz/moviefun/movies/MoviesDatabaseConfig.java
@@ -9,6 +9,7 @@ import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.superbiz.moviefun.DatabaseConfig;
 import org.superbiz.moviefun.DatabaseServiceCredentials;
 
 import javax.persistence.EntityManagerFactory;
